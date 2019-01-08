4f605a95c66b83d2f8de4404ec466a51a0badd88
Unknown <santiago.espinosa14@pm.me>
2019-01-08 09:44:36 +0800

    Updates to have a cleaner look and better user interaction

--- ip_calc.py
+++ ip_calc.py
@@ -1,14 +1,17 @@
 import ipaddress
+import os
 
 def preferences(): #Sets user preferences
     batches = int(input("Number of Batches to create: "))
     if batches == 0:
         print("Hahaha. Very funny...")
+		input()
         exit()
+    os.system('cls' if os.name == 'nt' else 'clear')
     for x in range(batches):        
         while 1:
             ip_start=[]
-            print("Batch",x+1,"STARTING IP: ")
+            print("Batch",x+1,"STARTING IP: ", end="")
             ip_start.append(str(input()))
             if len(ip_start[x].split('.')) == 4:
                 break
@@ -18,7 +21,7 @@ def preferences(): #Sets user preferences
                 
         while 1:
             ip_end=[]
-            print("Batch",x+1,"ENDING IP: ")
+            print("Batch",x+1,"ENDING IP: ", end="")
             ip_end.append(str(input()))
             if len(ip_end[x].split('.')) == 4:
                 break
@@ -41,17 +44,17 @@ def listMaker(ip_start, ip_end):
     return ip_startaddress_list
 
 def main():
-    print("IP Address target file creator\nVer: 0.1")
-    ip_start, ip_end = preferences()
+    print("IP Address Target File Creator\nVer: 0.1")
+    
+    ip_start, ip_end = preferences() #set starting and ending IP
+    
     for y in range(len(ip_start)):
         ip_list=[]
         ip_list = listMaker(ip_start[y], ip_end[y])
-        print("ip_list:", ip_list)
         file_name = "batch_"+str(y)+".txt"
         f=open(file_name, 'w+')
         for g in ip_list:
-            print("g:",g)
-            f.write(str(g), "\n")
+            f.write(str(g)+"\n")
         f.close() 
     print("Done. IP list in script directory")
     input()
